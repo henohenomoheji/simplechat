406d5086a188403368187e3fb98513dab0cc64b7
SATOKO KANEMOTO <kttxoww@gmail.com>
2017-10-25 22:55:29 +0900

    Revert "コントローラーとモデルの追加"
    This reverts commit 7155ac02e349f22207b380c5199d475dabece5d6.

--- app/controllers/messages_controller.rb
+++
@@ -1,9 +0,0 @@
-class MessagesController < ApplicationController
-  def index
-    @messages = Message.all
-    @msg = 'Hello'
-  end
-
-  def show
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  root to: 'messages#index'
-  #get 'messages/index'
-
-  get 'messages/show'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
